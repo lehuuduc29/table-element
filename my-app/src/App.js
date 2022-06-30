b65b10463570938fc8cfdfaf58c3a4787759c0cb
Le Huu Duc <95484394+lehuuduc29@users.noreply.github.com>
2022-07-01 01:04:27 +0700

    table element

---
+++ my-app/src/App.js
@@ -0,0 +1,56 @@
+
+import './App.css';
+
+
+
+const students = [
+  {
+    company: 'Alfreds Futterkiste',
+    contact: 'Maria Anders',
+    country: 'Germany'
+  },
+  {
+    company: 'Centro comercial Moctezuma',
+    contact: 'Francisco Chang',
+    country: 'Mexico'
+  },
+  {
+    company: 'Ernst Handel',
+    contact: 'Roland Mendel',
+    country: 'Austria'
+  },
+  {
+    company: 'Island Trading',
+    contact: 'Helen Bennett',
+    country: 'UK'
+  },
+  {
+    company: 'Laughing Bacchus Winecellars',
+    contact: 'Yoshi Tannamuri',
+    country: 'Canada'
+  },
+  {
+    company: 'Magazzini Alimentari Riuniti',
+    contact: 'Giovanni Rovelli',
+    country: 'Italy'
+  }
+]
+
+function App() {
+  return (
+    <div className="App">
+      <table>
+        {students.map(student => (
+            <tr>
+              <td>{student.company}</td>
+              <td>{student.contact}</td>
+              <td>{student.country}</td>
+            </tr>
+          ))}
+      </table>
+     
+    </div>
+  );
+}
+
+export default App;
